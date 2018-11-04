168cfb7c9b2cde6c4457b0b98b4aa1cfcdacd6c3
ckho <me@ckho.info>
2018-11-04 19:15:27 +0800

    Remove unused routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,9 @@
 import express from 'express';
-import indexRouter from './index.route';
-import usersRouter from './users.route';
 import authRouter from './auth.route';
 import checkinRouter from './checkin.route';
 import luckyDrawRouter from './luckydraw.route';
-// import projectRouter from './project.route';
- 
  
 const router = express.Router();
- 
-router.use('/', indexRouter);
 router.use('/api/auth', authRouter);
 router.use('/api/checkin', checkinRouter);
 router.use('/api/luckydraw', luckyDrawRouter);
