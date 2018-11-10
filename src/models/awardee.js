c650c1fad0a67542c64d06f32eabcd28b5e9629d
ckho <me@ckho.info>
2018-11-11 01:14:06 +0800

    Remove associated id in attr part

--- src/models/awardee.js
+++ src/models/awardee.js
@@ -1,8 +1,6 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Awardee = sequelize.define('Awardee', {
-    userId: DataTypes.INTEGER,
-    awardId: DataTypes.INTEGER
   }, {});
   Awardee.associate = function(models) {
     Awardee.belongsTo(models.User);
