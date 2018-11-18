e66f76c2e943b111a8ad0e16cc083db10a31fedb
ckho <me@ckho.info>
2018-11-18 16:36:45 +0800

    Better UX in Page Control

--- src/routes/page.route.js
+++ src/routes/page.route.js
@@ -5,6 +5,8 @@ import passportManager from '../config/passport';
 const router = express.Router();
 
 router.get('/list', passportManager.authenticate, pageController.index);
+router.get('/currentPage', passportManager.authenticate, pageController.currentPage);
+router.get('/id/:pageId', passportManager.authenticate, pageController.showId);
 router.get('/:pageName', passportManager.authenticate, pageController.show);
  
 export default router;
